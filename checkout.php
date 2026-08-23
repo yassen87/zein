8afc8a7e956d6072b5609549653a0b1b2b0eade2
Zei Perfumes <admin@zeiperfumes.com>
2026-08-23 18:33:40 +0300

    Add no-cache headers in checkout.php for instant mobile refresh

--- checkout.php
+++ checkout.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 require __DIR__ . '/includes/config.php';
 require __DIR__ . '/includes/products.php';
 require_once __DIR__ . '/includes/db.php';
